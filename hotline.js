d8b6b495b6c13a525e9914439ad0f7c42f00c275
sabrina <sabrinasultana381@gmail.com>
2025-08-28 23:16:15 +0600

    first commit

---
+++ hotline.js
@@ -0,0 +1,6 @@
+const callBtn = document.getElementById("call-btn");
+
+callBtn.addEventListener("click" , function(){
+   alert('<i class="fa-solid fa-phone"> Calling National Emergency Number')
+
+})
